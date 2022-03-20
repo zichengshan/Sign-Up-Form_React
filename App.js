046aaac1fd765d9ab6e342bbe4f9cf8c9aa48f1b
Zicheng.S <zichengshan@gmail.com>
2022-03-20 11:19:38 -0700

    FInished

--- App.js
+++ App.js
@@ -17,9 +17,31 @@ export default function App() {
      *    the "newsletter" checkbox, log "Thanks for signing
      *    up for our newsletter!" to the console.
      */
-    
+    const [formData, setFormData] = React.useState({
+        email: "",
+        passwords:"",
+        confirmPasswords:"",
+        join:true
+    })
+
+    function handleChange(event){
+        const {name, value, type, checked} = event.target
+        setFormData(prevFormData => {
+            return {
+                ...prevFormData,
+                [name]: type === "checkbox" ? checked : value
+            }
+        })
+    }
+
+
     function handleSubmit(event) {
         event.preventDefault()
+        alert("hello")
+        formData.passwords === formData.confirmPasswords ? console.log("Successfully signed up") : console.log("passwords to not match");
+        if(formData.join){
+            console.log("Thanks for signing up for our newsletter!")
+        }
     }
     
     return (
@@ -29,22 +51,34 @@ export default function App() {
                     type="email" 
                     placeholder="Email address"
                     className="form--input"
+                    name = "email"
+                    value = {formData.email}
+                    onChange = {handleChange}
                 />
                 <input 
                     type="password" 
                     placeholder="Password"
                     className="form--input"
+                    name = "passwords"
+                    value = {formData.password}
+                    onChange = {handleChange}
                 />
                 <input 
                     type="password" 
                     placeholder="Confirm password"
                     className="form--input"
+                    name = "confirmPasswords"
+                    value = {formData.confirmPassword}
+                    onChange = {handleChange}
                 />
                 
                 <div className="form--marketing">
                     <input
                         id="okayToEmail"
                         type="checkbox"
+                        name = "join"
+                        checked = {formData.join}
+                        onChange = {handleChange}
                         
                     />
                     <label htmlFor="okayToEmail">I want to join the newsletter</label>
